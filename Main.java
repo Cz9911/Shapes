fb77e94d87d0fa341ddb21a7b553cd7c6655668e
Cz9911 <>
2021-05-18 00:05:24 +0000

    I added the aspects to the triangle relating area and perimeter.

--- Main.java
+++ Main.java
@@ -5,6 +5,14 @@ class Main {
     double c = circle1.getCircumference();
     System.out.printf("circumference = %.2f\n", c);
     double a = circle1.getArea();
-    System.out.printf("Area = %.2f", a);
+    System.out.printf("Area = %.2f\n", a);
+    //Triangle stuff
+    Triangle traingle1 = new Triangle(27, "Blue");
+    System.out.println(traingle1);
+    double a2 = traingle1.getArea();
+    System.out.printf("Area = %.2f\n", a2);
+    double p = traingle1.getPerimeter();
+    System.out.printf("Perimeter = %.2f\n", p)
+    
   }
 }
---
+++ Triangle.java
@@ -0,0 +1,20 @@
+class Triangle{
+  //Fields
+  private int sideLs;
+  private String color;
+  //Constructors
+  public Triangle(int sideLs, String color){
+    this.color = color;
+    this.sideLs = sideLs;
+  } 
+  //toString
+  public String toString(){
+    return color + " " + sideLs;
+  }
+  public double getArea(){
+    return Math.sqrt(3) / 4 * Math.pow(sideLs, 2);
+  }
+  public double getPerimeter(){
+    return sideLs + sideLs + sideLs;
+  }
+}
